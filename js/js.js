9b778aa0fbc61b4365800d088124d36703336670
DandaryD <dr.dandary23@gmail.com>
2024-11-20 13:26:42 -0300

    criando arquivo js

---
+++ js/js.js
@@ -0,0 +1,12 @@
+var txt2 = "Testando O guit Hub "
+
+alert (txt2); //exibe um console no navegador
+a
+
+//------------------------------------------------------------------
+var txt = "Aula 07 - Entendendo variáveis- Testando Java Script - Arquivo Externo com o código alert"
+var nome = "Manoel",
+    idade = 39,
+    resultado = "O cliente " + nome + "tem " + idade + " anos.";
+
+console.log (txt, resultado);
